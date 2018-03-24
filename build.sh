4d87d62ab914f250825f5e5fe42cb4629bdc72cc
Xavier Mayeur <xavier@mayeur.be>
2018-03-24 23:03:23 +0100

    Add tokens in vault

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 git pull
 docker rm -f monfreezer
 docker build -t monfreezer . && \
-docker tag monfreezer xmayeur/monfreezer && \
+docker tag monfreezer xmayeur/monfreezer
+
+chmod +x monfreezer.sh
+sudo cp MonFreezeDoor.conf monfreeze.sh /root
 /bin/sh ./monfreezer.sh
 
 #docker push xmayeur/monfreezer && \
